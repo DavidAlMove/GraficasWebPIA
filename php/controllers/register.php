0a3124efbdc43ea71f3af6f0e2b40d3dd0844d57
David Montemayor <david.montemayor@netpay.com.mx>
2021-11-24 18:29:58 -0600

    a

--- php/controllers/register.php
+++ php/controllers/register.php
@@ -7,15 +7,18 @@
 	$action = $_POST['vAction'];
 
 	if($action == "I"){
+
 		$username = $_POST['username'];
 		$user_name = $_POST['user_name'];
 		$email = $_POST['user_email'];
 		$password = $_POST['user_password'];
 
-		$resp = Register::registerUser($username, $user_name, $email, $password);
+		print_r($_POST);
+
+		//$resp = Register::registerUser($username, $user_name, $email, $password);
 	}
 	else if($action == 'SA'){
-		
+
 	}
 
 	echo json_encode($resp);
